f468bcc774a0f86f56264ffff691205d6917407d
DaemonAndrey <robinperez38@gmail.com>
2016-02-28 20:37:26 -0600

    Corregido el script de la base de datos

--- Backup/Mysql/database.sql
+++ Backup/Mysql/database.sql
@@ -1,10 +1,10 @@
 CREATE TABLE associations
 (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL UNSIGNED AUTO_INCREMENT,
   acronym varchar(16) NOT NULL,
   name varchar(70) NOT NULL,
   location varchar(100) DEFAULT NULL,
   schedule varchar(100) DEFAULT NULL,
-  authorized_card BIT(1) DEFAULT 0 NOT NULL,
+  authorized_card INT(1) DEFAULT 0 NOT NULL,
   PRIMARY KEY(id)
 );
