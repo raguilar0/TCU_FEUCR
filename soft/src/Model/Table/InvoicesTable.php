100d9798f929a9268f70f14a1961ae6056ced9ff
José Slon Baltodano <jslon0891@gmail.com>
2016-06-18 21:03:12 -0600

    Validación agregar factura

--- soft/src/Model/Table/InvoicesTable.php
+++ soft/src/Model/Table/InvoicesTable.php
@@ -11,23 +11,58 @@ class InvoicesTable extends Table
     {
         $this->addBehavior('Timestamp');
         $this->belongsTo('Associations');
-    
+
     }
 
     public function validationDefault(Validator $validator)
     {
-        $validator      
-            ->requirePresence('number')
-            ->requirePresence('amount')
-            ->requirePresence('clarifications')
-            ->requirePresence('detail')
-            ->requirePresence('kind')
-            ->requirePresence('attendant');
+      $validator
+          ->requirePresence('number')
+          ->add('number', 'validFormat', [
+                      'rule' => array('custom', '/[0-9]+$/'),
+                      'message' => 'Formato inválido. Solo números.'
+          ])
+          ->requirePresence('amount')
+          ->add('amount', 'validFormat', [
+                      'rule' => array('custom', '/^[0-9]+$/'),
+                      'message' => 'Formato inválido. Solo números.'
+          ])
+          ->requirePresence('clarifications')
+          ->add('clarifications', 'validFormat', [
+                      'rule' => array('custom', '/^[A-Za-z0-9\.\,\-\:]+$/'),
+                      'message' => 'Formato inválido'
+          ])
+          ->requirePresence('detail')
+          ->add('detail', 'validFormat', [
+                      'rule' => array('custom', '/^[A-Za-z0-9\.\,\-\:]+$/'),
+                      'message' => 'Formato inválido'
+          ])
+          ->requirePresence('kind')
+          ->add('kind', 'validFormat', [
+                      'rule' => array('custom', '/^[A-Za-z0-9]+$/'),
+                      'message' => 'Formato inválido'
+          ])
+          ->requirePresence('attendant')
+          ->add('attendant', 'validFormat', [
+                      'rule' => array('custom', '/^[A-Za-z]+$/'),
+                      'message' => 'Formato inválido'
+          ])
+          ->requirePresence('provider')
+          ->add('provider', 'validFormat', [
+                      'rule' => array('custom', '/^[A-Za-z]+$/'),
+                      'message' => 'Formato inválido'
+          ])
+          ->requirePresence('legal_certificate')
+          ->add('legal_certificate', 'validFormat', [
+                      'rule' => array('custom', '/^[0-9\-]+$/'),
+                      'message' => 'Formato inválido'
+          ])
+          ;
 
 
-        return $validator;
+      return $validator;
     }
-	
-	
-	
+
+
+
 }
